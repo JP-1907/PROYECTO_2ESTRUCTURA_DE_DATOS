10f3824d756c444352cc13cfe3e321151cde5b42
JP-1907 <juanpablorojasvargas07@gmail.com>
2026-03-25 16:47:18 -0400

    Update GestorImpresion.java

--- Proyecto 2/src/proyecto/pkg2/GestorImpresion.java
+++ Proyecto 2/src/proyecto/pkg2/GestorImpresion.java
@@ -4,16 +4,15 @@ public class GestorImpresion {
     private MonticuloBinario cola = new MonticuloBinario();
     private TablaHash usuarios = new TablaHash();
 
-    // Este método lo llama tu botón "Añadir"
+
     public void agregarDocumento(String nombre, String usuario, boolean forzarPrio, int tam) {
         int prioridadFinal;
         String tipoEnTabla = usuarios.buscarTipo(usuario);
 
-        // Lógica de prioridad: Si es VIP o se marcó el CheckBox, prioridad alta (valor bajo)
         if (forzarPrio || tipoEnTabla.equalsIgnoreCase("prioridad_alta")) {
             prioridadFinal = (tam < 10) ? tam : 5; 
         } else {
-            prioridadFinal = 100 + tam; // Prioridad baja
+            prioridadFinal = 100 + tam; 
         }
 
         Documento nuevo = new Documento(nombre, usuario, prioridadFinal);
